a437903ff8c022ce90ad27d3d3fd6cb921d295ce
javiermruelas <javiermruelas@gmail.com>
2023-04-09 16:49:09 -0700

    configured root alias path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
+import { fileURLToPath } from "url";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    alias: {
+        '@': fileURLToPath(new URL('./', import.meta.url))
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+    // this whole site needs to use dark blue (serious, mature, dependable)
+</script>
+
 <template>
     <h1>Hello World!</h1>
 </template>
